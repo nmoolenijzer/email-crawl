8950ae8baee03cabc247fbfe0f19d131b581b641
Nick Moolenijzer <nick@moolenijzer.com>
2016-01-05 11:33:30 -0500

    crawler

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,18 +1,23 @@
 import java.util.*;
 import java.io.*;
+import java.net.URL;
 
 public class Crawler {
 	
 	private HashSet<String> emails;
-	private String filePath;
-	private FileReader inputHTML;
+	private URL initURL;
+	private InputStream inputStream;
+	private BufferedReader bufferedReader;
+	private BufferedReader inputHTML;
 	
 	public Crawler(String file) {
 		emails = new HashSet<String>();
-		filePath = file;
 		try {
-			inputHTML = new FileReader(filePath);
-		} catch (FileNotFoundException e) {
+			initURL = new URL(file);
+			inputStream = initURL.openStream();
+			inputHTML = new BufferedReader(new InputStreamReader(inputStream));
+		} catch (IOException e) {
+			e.printStackTrace();
 			System.err.println("Error loading website.");
 		}
 	}
@@ -28,26 +33,22 @@ public class Crawler {
 	}
 	
 	public void readEmails() throws IOException {
-		try {
-			int curr = inputHTML.read();
-			while (curr != -1) {
-				String currWord = "";
-				while ((char)curr == ' ') {
-					curr = inputHTML.read();
-				}
-				while ((char)curr != ' ' && curr != 10 && curr != 9 && curr != -1) {
-					currWord += (char)curr;
-					curr = inputHTML.read();
-				}
-				if (currWord.contains("@")) {
-					this.addEmail(currWord);
-				}
+		int curr = inputHTML.read();
+		while (curr != -1) {
+			String currWord = "";
+			while ((char)curr == ' ') {
 				curr = inputHTML.read();
-				
 			}
-		}
-		finally {
-			inputHTML.close();
+			while ((char)curr != ' ' && curr != 10 && curr != 9 && curr != -1) {
+				currWord += (char)curr;
+				curr = inputHTML.read();
+			}
+			System.out.println(currWord);
+			if (currWord.contains("@")) {
+				this.addEmail(currWord);
+			}
+			curr = inputHTML.read();
+			
 		}
 	}
 	
@@ -58,7 +59,7 @@ public class Crawler {
 		}
 		
 		else {
-			Crawler myCrawler = new Crawler("../about.html");
+			Crawler myCrawler = new Crawler("http://jana.com/contact");
 			try {
 				myCrawler.readEmails();
 				myCrawler.printEmails();
