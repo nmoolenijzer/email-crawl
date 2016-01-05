7c253bc5bf904e1b27574a5a714078c615c5e657
Nick Moolenijzer <nick@moolenijzer.com>
2016-01-05 18:06:24 -0500

    fin w/o comments

--- src/Crawler.java
+++ src/Crawler.java
@@ -1,25 +1,21 @@
 import java.util.*;
 import java.io.*;
 import java.net.URL;
+import java.net.URLConnection;
 
 public class Crawler {
-	
+
 	private HashSet<String> emails;
-	private URL initURL;
-	private InputStream inputStream;
-	private BufferedReader bufferedReader;
-	private BufferedReader inputHTML;
+	private HashSet<String> visited;
+	private Queue<String> domains;
+	private String initDomain;
 	
 	public Crawler(String file) {
+		initDomain = file;
 		emails = new HashSet<String>();
-		try {
-			initURL = new URL(file);
-			inputStream = initURL.openStream();
-			inputHTML = new BufferedReader(new InputStreamReader(inputStream));
-		} catch (IOException e) {
-			e.printStackTrace();
-			System.err.println("Error loading website.");
-		}
+		visited = new HashSet<String>();
+		domains = new PriorityQueue<String>();
+		domains.add("http://" + file);
 	}
 	
 	public void addEmail(String newArg) {
@@ -33,23 +29,127 @@ public class Crawler {
 	}
 	
 	public void readEmails() throws IOException {
-		int curr = inputHTML.read();
-		while (curr != -1) {
-			String currWord = "";
-			while ((char)curr == ' ') {
-				curr = inputHTML.read();
+		URL initURL;
+		InputStream inputStream;
+		BufferedReader inputHTML;
+		while (!domains.isEmpty()) {
+			String file = domains.remove();
+			if (!visited.contains(file)) {
+				try {
+					initURL = new URL(file);
+					inputStream = initURL.openStream();
+					inputHTML = new BufferedReader(new InputStreamReader(inputStream));
+					
+					int curr = inputHTML.read();
+					
+					while (curr != -1) {
+						String currWord = "";
+						while ((char)curr == ' ') {
+							curr = inputHTML.read();
+						}
+						while ((char)curr != ' ' && curr != 10 && curr != 9 && curr != -1) {
+							currWord += (char)curr;
+							curr = inputHTML.read();
+						}
+						if (validEmail(currWord)) {
+							if (currWord.length() > 0 && currWord.contains("\"")) {
+								while (currWord.length() > 1 && currWord.charAt(0) != '"') {
+									currWord = currWord.substring(1, currWord.length());
+								}
+								currWord = currWord.substring(1, currWord.length());
+								int i = 0;
+								while (i < currWord.length() && currWord.charAt(i) != '"') { i++; }
+								currWord = currWord.substring(0, i);
+							}
+							if (currWord.length() > 0 && currWord.contains("<")) {
+								while (currWord.length() > 1 && currWord.charAt(0) != '<') {
+									currWord = currWord.substring(1, currWord.length());
+								}
+								currWord = currWord.substring(1, currWord.length());
+								int i = 0;
+								while (i < currWord.length() && currWord.charAt(i) != '>') { i++; }
+								currWord = currWord.substring(0, i);
+							}
+							if (currWord.length() > 0 && currWord.contains(":")) {
+								while (currWord.length() > 1 && currWord.charAt(0) != ':') {
+									currWord = currWord.substring(1, currWord.length());
+								}
+								currWord = currWord.substring(1, currWord.length());
+							}
+							if (secondCheck(currWord)) {
+								this.addEmail(currWord);
+							}
+						}
+						else if (validDomain(currWord)) {
+							if (currWord.length() > 0 && currWord.contains("\"")) {
+								while (currWord.length() > 1 && currWord.charAt(0) != '"') {
+									currWord = currWord.substring(1, currWord.length());
+								}
+								currWord = currWord.substring(1, currWord.length());
+								int i = 0;
+								while (i < currWord.length() && currWord.charAt(i) != '"') { i++; }
+								currWord = currWord.substring(0, i);
+							}
+							if (currWord.length() > 0 && currWord.contains("<")) {
+								while (currWord.length() > 1 && currWord.charAt(0) != '<') {
+									currWord = currWord.substring(1, currWord.length());
+								}
+								currWord = currWord.substring(1, currWord.length());
+								int i = 0;
+								while (i < currWord.length() && currWord.charAt(i) != '>') { i++; }
+								currWord = currWord.substring(0, i);
+							}
+							if (validDomain(currWord) && (currWord.contains("http://") || currWord.contains("https://"))) {
+								domains.add(currWord);
+							}
+							else if (validDomain(currWord)) {
+								domains.add("http://" + currWord);
+							}
+						}
+						curr = inputHTML.read();
+					}
+					visited.add(file);
+					inputStream.close();
+				}
+				catch (IOException e) {
+					System.err.println("Error loading website: " + file);
+				}
 			}
-			while ((char)curr != ' ' && curr != 10 && curr != 9 && curr != -1) {
-				currWord += (char)curr;
-				curr = inputHTML.read();
-			}
-			System.out.println(currWord);
-			if (currWord.contains("@")) {
-				this.addEmail(currWord);
-			}
-			curr = inputHTML.read();
-			
 		}
+	}
+	
+	public boolean validEmail(String word) {
+		if (word.contains("@") && valid(word)) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean secondCheck(String word) {
+		if (word.endsWith(".com") || word.endsWith(".edu") ||
+				word.endsWith(".org") || word.endsWith(".net") ||
+				word.endsWith(".gov") || word.endsWith(".int") ||
+				word.endsWith(".mil")) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean validDomain(String currWord) {
+		if (currWord.contains(initDomain) && !currWord.contains("." + initDomain)) {
+			return true;
+		}
+		else return false;
+	}
+	
+	public boolean valid(String word) {
+		if (word.contains(".com") || word.contains(".edu") ||
+				word.contains(".org") || word.contains(".net") ||
+				word.contains(".gov") || word.contains(".int") ||
+				word.contains(".mil")) {
+			return true;
+		}
+		else return false;
 	}
 	
 	public static void main(String args[]) {
@@ -59,7 +159,7 @@ public class Crawler {
 		}
 		
 		else {
-			Crawler myCrawler = new Crawler("http://jana.com/contact");
+			Crawler myCrawler = new Crawler(args[0]);
 			try {
 				myCrawler.readEmails();
 				myCrawler.printEmails();
